1dd313d1d045db026a4dee20c0a33bfceddaf862
Kunal Das <kunal.dusk@gmail.com>
2025-08-01 11:51:23 +0530

    add welcome-screen, setting-dialog, & browser window comfig

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,19 @@
+import { useCallback, useState } from "react";
 import { Toaster } from "./components/ui/sonner";
 import Versions from "./components/Versions";
+import { WelcomeScreen } from "./components/WelcomeScreen";
 import { QueryProvider } from "./providers/query.provider";
 
 function App(): React.JSX.Element {
+    const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+
+    const onOpenSettings = useCallback(() => {}, []);
     return (
         <QueryProvider>
             <Toaster closeButton richColors />
-            <Versions />
+            <div className="relative">
+                <WelcomeScreen onOpenSettings={onOpenSettings} />
+            </div>
         </QueryProvider>
     );
 }
--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -1,17 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { toast } from "sonner";
 
 function Versions(): React.JSX.Element {
-    useEffect(() => {
-        toast.success("Great success");
-        setTimeout(() => {
-            toast.error("Very bad error");
-        }, 3000);
-        setTimeout(() => {
-            toast.message("No worries");
-        }, 3000);
-    }, []);
-
     return (
         <div>
             <h1 className="text-2xl font-semibold text-emerald-600">Hello</h1>
